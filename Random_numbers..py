134f6dcc5146e6d34b052aafab4912fbf0844e48
Baauf-02 <112335593+Baauf-02@users.noreply.github.com>
2022-08-30 19:24:24 +0700

    Add files via upload

---
+++ Random_numbers..py
@@ -0,0 +1,13 @@
+import random
+def random_number_from_100_to_10():
+    my_list=[]
+    even_number=0
+    for i in range(100):
+        x= random.randint(1,101)
+        my_list.append(x)
+    print(my_list)
+    for i in my_list:
+        if i%2==0:
+            even_number+=1
+    print("You have %s even numbers!"%even_number)
+random_number_from_100_to_10()
